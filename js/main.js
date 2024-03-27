71f603dcb51452fe8752b763971d18a0be63515f
suminkim <di05023@gmail.com>
2024-03-27 18:06:34 +0900

    벡터 색상 변경

--- js/main.js
+++ js/main.js
@@ -122,43 +122,40 @@ window.onload = function () {
       });
 
       // 이전에 활성화된 메뉴들을 비활성화합니다.
-      document
-        .querySelectorAll(".sd-wrapper > li > a.sdw-focus")
-        .forEach(function (menu) {
-          menu.classList.remove("sdw-focus");
-        });
+      document.querySelectorAll(".sd-wrapper > li > a.sdw-focus").forEach(function (menu) {
+        menu.classList.remove("sdw-focus");
+      });
 
       // 현재 메뉴를 활성화합니다.
       element.classList.add("sdw-focus");
     });
   });
-// 각 섹션의 위치를 계산합니다.
-const sections = document.querySelectorAll(".sd-wrapper > li > a");
-const sectionOffsets = Array.from(sections).map(section => {
-  const sectionId = section.getAttribute("href").substring(1);
-  const targetSection = document.getElementById(sectionId);
-  return {
-    id: sectionId,
-    offsetTop: targetSection.offsetTop,
-    offsetBottom: targetSection.offsetTop + targetSection.offsetHeight
-  };
-});
-
-// 스크롤 이벤트를 처리합니다.
-window.addEventListener("scroll", function() {
-  const scrollPosition = window.scrollY;
-
-  // 스크롤 위치에 따라 현재 섹션을 확인하고 해당하는 메뉴를 활성화합니다.
-  sectionOffsets.forEach(section => {
-    const menuLink = document.querySelector(`.sd-wrapper > li > a[href="#${section.id}"]`);
-    if (scrollPosition >= section.offsetTop && scrollPosition < section.offsetBottom) {
-      menuLink.classList.add("sdw-focus");
-    } else {
-      menuLink.classList.remove("sdw-focus");
-    }
+  // 각 섹션의 위치를 계산합니다.
+  const sections = document.querySelectorAll(".sd-wrapper > li > a");
+  const sectionOffsets = Array.from(sections).map((section) => {
+    const sectionId = section.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(sectionId);
+    return {
+      id: sectionId,
+      offsetTop: targetSection.offsetTop,
+      offsetBottom: targetSection.offsetTop + targetSection.offsetHeight,
+    };
   });
-});
 
+  // 스크롤 이벤트를 처리합니다.
+  window.addEventListener("scroll", function () {
+    const scrollPosition = window.scrollY;
+
+    // 스크롤 위치에 따라 현재 섹션을 확인하고 해당하는 메뉴를 활성화합니다.
+    sectionOffsets.forEach((section) => {
+      const menuLink = document.querySelector(`.sd-wrapper > li > a[href="#${section.id}"]`);
+      if (scrollPosition >= section.offsetTop && scrollPosition < section.offsetBottom) {
+        menuLink.classList.add("sdw-focus");
+      } else {
+        menuLink.classList.remove("sdw-focus");
+      }
+    });
+  });
 
   //ani product link
   var fnScNavLinkElement = document.querySelector(".ani-product a");
@@ -186,6 +183,18 @@ window.addEventListener("scroll", function() {
   });
 
   //quiz area
+  //animation
+  var playButton = document.querySelectorAll(".svg-title");
+
+  playButton.forEach(function (button) {
+    button.addEventListener("click", function () {
+      // [재생] 버튼을 클릭하면 -webkit-animation-play-state: running 속성 적용
+      var svgs = document.querySelectorAll(".main-svg-banner svg");
+      svgs.forEach(function (svg) {
+        svg.style.animationPlayState = "running";
+      });
+    });
+  });
   AOS.init();
   const fnAnswerBox = document.querySelectorAll(".answer-box .answer-box-li");
   const fnChoiceBox = document.querySelectorAll(".choice-box .choice-box-li");
@@ -200,9 +209,7 @@ window.addEventListener("scroll", function() {
       fnAnswerBox[currentIdx].classList.add("jsAnswer");
       currentIdx++;
       // 모든 fnAnswerBox가 채워졌는지 확인하고 정답 확인 및 버튼 활성화/비활성화
-      const isAllFilled = Array.from(fnAnswerBox).every(
-        (li) => li.textContent.trim().length > 0
-      );
+      const isAllFilled = Array.from(fnAnswerBox).every((li) => li.textContent.trim().length > 0);
       if (isAllFilled) {
         fnChkAnswerSub.classList.add("active-as");
       }
@@ -312,13 +319,7 @@ window.addEventListener("scroll", function() {
         draw: function () {
           ctx.fillStyle = "#fff";
           ctx.beginPath();
-          ctx.arc(
-            this.x,
-            this.y,
-            this.r,
-            -HALF_PI,
-            TWO_PI * this._progress - HALF_PI
-          );
+          ctx.arc(this.x, this.y, this.r, -HALF_PI, TWO_PI * this._progress - HALF_PI);
           ctx.lineTo(this.x, this.y);
           ctx.closePath();
           ctx.fill();
@@ -354,13 +355,7 @@ window.addEventListener("scroll", function() {
         draw: function () {
           ctx.fillStyle = "#fff";
           ctx.beginPath();
-          ctx.arc(
-            this.x,
-            this.y,
-            this.startRadius * (1 - this.progress),
-            0,
-            TWO_PI
-          );
+          ctx.arc(this.x, this.y, this.startRadius * (1 - this.progress), 0, TWO_PI);
           ctx.fill();
         },
       };
@@ -391,14 +386,8 @@ window.addEventListener("scroll", function() {
       function createParticles() {
         for (var i = 0; i < 128; i++) {
           var p0 = new Point(viewWidth * 0.5, viewHeight * 0.5);
-          var p1 = new Point(
-            Math.random() * viewWidth,
-            Math.random() * viewHeight
-          );
-          var p2 = new Point(
-            Math.random() * viewWidth,
-            Math.random() * viewHeight
-          );
+          var p1 = new Point(Math.random() * viewWidth, Math.random() * viewHeight);
+          var p2 = new Point(Math.random() * viewWidth, Math.random() * viewHeight);
           var p3 = new Point(Math.random() * viewWidth, viewHeight + 64);
 
           particles.push(new Particle(p0, p1, p2, p3));
@@ -501,16 +490,8 @@ window.addEventListener("scroll", function() {
         var p = new Point();
         var nt = 1 - t;
 
-        p.x =
-          nt * nt * nt * p0.x +
-          3 * nt * nt * t * c0.x +
-          3 * nt * t * t * c1.x +
-          t * t * t * p1.x;
-        p.y =
-          nt * nt * nt * p0.y +
-          3 * nt * nt * t * c0.y +
-          3 * nt * t * t * c1.y +
-          t * t * t * p1.y;
+        p.x = nt * nt * nt * p0.x + 3 * nt * nt * t * c0.x + 3 * nt * t * t * c1.x + t * t * t * p1.x;
+        p.y = nt * nt * nt * p0.y + 3 * nt * nt * t * c0.y + 3 * nt * t * t * c1.y + t * t * t * p1.y;
 
         return p;
       }
@@ -518,17 +499,15 @@ window.addEventListener("scroll", function() {
       requestAnimationFrame(loop);
     } else {
       document.querySelector(".qbg3").classList.add("show-qbg3");
-      document
-        .querySelector(".quiz-retry-btn")
-        .addEventListener("click", function () {
-          document.querySelector(".qbg3").classList.remove("show-qbg3");
-          // 틀렸을 경우 정답 박스 초기화
-          currentIdx = 0;
-          fnAnswerBox.forEach((box) => {
-            box.textContent = "";
-            box.classList.remove("jsAnswer");
-          });
+      document.querySelector(".quiz-retry-btn").addEventListener("click", function () {
+        document.querySelector(".qbg3").classList.remove("show-qbg3");
+        // 틀렸을 경우 정답 박스 초기화
+        currentIdx = 0;
+        fnAnswerBox.forEach((box) => {
+          box.textContent = "";
+          box.classList.remove("jsAnswer");
         });
+      });
     }
   });
 
